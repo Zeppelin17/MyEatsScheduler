f89682eee9345fcfe827273000823f81c69d6b7e
Zeppelin17 <polmonclus@outlook.com>
2020-04-20 06:40:12 +0200

    Add some fields in models.

--- backend/myeats/myeats_recipes/models.py
+++ backend/myeats/myeats_recipes/models.py
@@ -5,7 +5,7 @@
  * @author Zeppelin17 <elzeppelin17@gmail.com>
  *
  * Created at     : 2020-04-15 06:47:49 
- * Last modified  : 2020-04-16 06:50:57
+ * Last modified  : 2020-04-19 10:53:32
  */"""
 
 from django.db import models
@@ -14,6 +14,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    color = models.CharField(max_length=7, blank=True)
+    myeats_user = models.ForeignKey('myeats_user.MyEatsUser', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -24,8 +26,8 @@ class Recipe(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True)
     steps = models.TextField(blank=True)
-    categories = models.ManyToManyField(Category)
-
+    categories = models.ManyToManyField(Category, blank=True)
+    myeats_user = models.ForeignKey('myeats_user.MyEatsUser', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- backend/myeats/myeats_scheduler/models.py
+++ backend/myeats/myeats_scheduler/models.py
@@ -5,7 +5,7 @@
  * @author Zeppelin17 <elzeppelin17@gmail.com>
  *
  * Created at     : 2020-04-14 06:24:56 
- * Last modified  : 2020-04-14 06:53:15
+ * Last modified  : 2020-04-19 09:43:59
  */"""
 
 from django.db import models
@@ -18,8 +18,8 @@ FIRST_DAY_CHOICES = [
 class Week(models.Model):
     name = models.CharField(max_length=150)
     first_day = models.CharField(max_length=3, choices=FIRST_DAY_CHOICES)
-    from_date = models.DateField(blank=True)
-    to_date = models.DateField(blank=True)
+    from_date = models.DateField(blank=True, null=True)
+    to_date = models.DateField(blank=True, null=True)
 
     myeats_user = models.ForeignKey('myeats_user.MyEatsUser', on_delete=models.CASCADE)
 
@@ -44,7 +44,7 @@ class Split(models.Model):
     name = models.CharField(max_length=20)
     order = models.PositiveIntegerField()
     day = models.ForeignKey(Day, on_delete=models.CASCADE)
-    recipes = models.ManyToManyField('myeats_recipes.Recipe')
+    recipes = models.ManyToManyField('myeats_recipes.Recipe', blank=True)
 
     def __str__(self):
         return self.name
