3baeec22861f529db545eab44bf7ff216b11d787
zeppelin17 <polmonclus@outlook.com>
2020-05-23 07:00:54 +0200

    Add logic to change template by prop 'isInFooter'

--- frontend/myeats/src/components/LocaleSwitcher.vue
+++ frontend/myeats/src/components/LocaleSwitcher.vue
@@ -12,33 +12,63 @@
 
 
 <template>
-    <div class="relative py-3">
-        <div 
-            v-if="showLangSelector"
-            class="fixed inset-0 w-full h-full bg-gray-500 bg-opacity-25 z-10"
-            @click="showLangSelector = !showLangSelector"
-        ></div>
-        <div 
-            class="flex cursor-pointer"
-            @click="showLangSelector = !showLangSelector"
-        >
-            <span class="text-blue-900">{{ this.$i18n.locale }}</span>
-            <img
-                class="w-3 text-blue-900"
-                :src="requireImage('caret-down-outline.svg')" 
-                :alt="$t('langMenu.selectDropdown')"
-            >
+
+    <div>
+        <!-- Template if the language switcher is not in the footer -->
+        <div v-if="!isInFooter">
+            <div class="relative py-3">
+                <div 
+                    v-if="showLangSelector"
+                    class="fixed inset-0 w-full h-full bg-gray-500 bg-opacity-25 z-10"
+                    @click="showLangSelector = !showLangSelector"
+                ></div>
+                <div 
+                    class="flex cursor-pointer"
+                    @click="showLangSelector = !showLangSelector"
+                >
+                    <span class="text-blue-900">{{ this.$i18n.locale }}</span>
+                    <img
+                        class="w-3 text-blue-900"
+                        :src="requireImage('caret-down-outline.svg')" 
+                        :alt="$t('langMenu.selectDropdown')"
+                    >
+                </div>
+                <div 
+                    v-if="showLangSelector"
+                    class="locale-list"
+                >
+                    <div class="locale-wrap">
+                        <div 
+                            class="locale"
+                            v-for="locale in locales" 
+                            :key="locale"
+                            @click="showLangSelector = !showLangSelector"
+                        >
+                            <a 
+                                class=""
+                                @click="switchLocale(locale, $event)"
+                                href="#"
+                            >
+                                <img 
+                                    class="mr-1 w-6 inline"
+                                    :src="requireImage($t('langMenu.' + locale + '.flag'))" 
+                                    :alt="$t('langMenu.' + locale + '.name')"
+                                >
+                                <span>{{ $t('langMenu.' + locale + '.name') }}</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div 
-            v-if="showLangSelector"
-            class="locale-list absolute right-0 w-32"
-        >
-            <div class="locale-wrap">
+
+        <!-- Template for the footer language switcher -->
+        <div v-if="isInFooter">
+            <div class="locale-wrap footer">
                 <div 
                     class="locale"
                     v-for="locale in locales" 
                     :key="locale"
-                    @click="showLangSelector = !showLangSelector"
                 >
                     <a 
                         class=""
@@ -62,6 +92,12 @@
 import { Trans } from '@/plugins/Translation'
 export default {
     name: "LocaleSwitcher",
+    props: {
+        isInFooter: {
+            type: Boolean,
+            default: false
+        }
+    },
     computed: {
         supportedLocales() {
             return Trans.supportedLocales
@@ -93,7 +129,7 @@ export default {
 
 <style scoped>
 .locale-list {
-    @apply z-20
+    @apply z-20 absolute right-0 w-32
 }
 .locale-list .locale-wrap {
     @apply flex flex-col items-start bg-blue-300 shadow-xl rounded
@@ -109,5 +145,34 @@ export default {
 }
 .locale-list .locale-wrap .locale:hover a img{
     @apply border-gray-200 border-2 rounded-full
+}
+
+
+
+.locale-wrap.footer {
+    @apply flex flex-col items-start
+}
+.locale-wrap.footer .locale {
+    @apply flex mx-auto w-32 rounded-lg text-gray-800 border-2 border-blue-400
+}
+.locale-wrap.footer .locale a {
+    @apply p-2 w-full rounded
+}
+.locale-wrap.footer .locale:hover {
+    @apply border-2 border-blue-900 text-blue-900
+}
+.locale-wrap.footer .locale:hover a img{
+    @apply border-blue-900 border-2 rounded-full
+}
+
+@media (min-width: 768px) {
+    
+    .locale-wrap.footer .locale a {
+        @apply text-left
+    }
+
+    .locale-wrap.footer .locale {
+        @apply w-full
+    }
 }
 </style>
