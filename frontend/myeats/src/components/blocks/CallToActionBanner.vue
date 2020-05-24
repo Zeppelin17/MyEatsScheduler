9863cf011e1281baba0421c5584d4904e78f5aa1
zeppelin17 <polmonclus@outlook.com>
2020-05-24 08:43:18 +0200

    Create and style CallToActionBanner component

---
+++ frontend/myeats/src/components/blocks/CallToActionBanner.vue
@@ -0,0 +1,111 @@
+<script>
+/**
+ * Block component to show a call to action banner
+ *
+ * @summary Block component to show a call to action banner
+ * @author Zeppelin17 <elzeppelin17@gmail.com>
+ *
+ * Created at     : 2020-05-23 17:42:10
+ * Last modified  : 2020-05-24 08:42:37
+ */
+</script>
+<template>
+  <div class="cta-banner">
+    <div class="cta-title">
+      <h2>{{ title }}</h2>
+    </div>
+    <div class="cta-buttons">
+      
+      <a 
+        v-for="button in allButtons"
+        :key="button.text"
+
+        :href="button.link"
+        :title="button.text"
+        class="cta-btn"
+      >
+        {{ button.text }}
+      </a>
+
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CallToActionBanner',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    button1Text: {
+      type: String,
+      required: true
+    },
+    button1Link: {
+      type: String,
+      required: true
+    },
+    button2Text: {
+      type: String,
+      default: null
+    },
+    button2Link: {
+      type: String,
+      default: null
+    }
+
+  },
+  computed: {
+    allButtons: function() {
+      let buttons = Array()
+      buttons.push({"text": this.button1Text, "link": this.button1Link})
+
+      if (this.button2Text !== null && this.button2Link !== null) {
+        buttons.push({"text": this.button2Text, "link": this.button2Link})
+      }
+
+      return buttons
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.cta-banner {
+  @apply flex items-center justify-between -mx-3 px-5 py-20 mb-16 bg-blue-900 text-blue-200 shadow-xl
+}
+
+.cta-banner .cta-title {
+  @apply mr-5
+}
+
+.cta-banner .cta-buttons {
+  @apply flex flex-col block
+}
+
+.cta-banner .cta-buttons {
+  @apply flex flex-col text-center
+}
+
+.cta-banner .cta-buttons .cta-btn {
+  @apply whitespace-no-wrap block px-4 py-3 my-2 bg-blue-200 text-blue-900 font-semibold text-sm rounded-lg border-2 border-blue-200
+}
+
+.cta-banner .cta-buttons .cta-btn:hover {
+  @apply border-blue-200 bg-blue-400 shadow-xl
+}
+
+@media (min-width: 640px) {
+  .cta-banner {
+    @apply -mx-12 px-12 py-24
+  }
+
+  .cta-banner .cta-buttons .cta-btn {
+    @apply px-6 py-4 text-lg
+  }
+}
+</style>
--- frontend/myeats/src/views/Home.vue
+++ frontend/myeats/src/views/Home.vue
@@ -6,7 +6,7 @@
  * @author Zeppelin17 <elzeppelin17@gmail.com>
  *
  * Created at     : 2020-04-29 17:23:47 
- * Last modified  : 2020-05-18 07:02:11
+ * Last modified  : 2020-05-24 08:42:47
  */
 </script>
 
@@ -23,6 +23,13 @@
       :content="$t('home.whatIs.text')"
       imageName="refreshing_beverage.svg"
       imageAlign="right" 
+    />
+
+    <CallToActionBanner
+      title="Start planning your meals"
+      button1Text="Create an account"
+      button1Link="/en/register"
+
     />
 
     <TextImageBlock 
@@ -45,12 +52,14 @@
 // @ is an alias to /src
 import TextImageBlock from '@/components/blocks/TextImageBlock.vue'
 import HeroImageBlock from '@/components/blocks/HeroImageBlock.vue'
+import CallToActionBanner from '@/components/blocks/CallToActionBanner.vue'
 
 export default {
   name: 'home',
   components: {
     TextImageBlock,
-    HeroImageBlock
+    HeroImageBlock,
+    CallToActionBanner
   }
 }
 </script>
