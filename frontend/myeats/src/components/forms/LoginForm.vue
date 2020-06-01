b50bbcb3562d9a1504e131da303aaa1b86bb9107
zeppelin17 <polmonclus@outlook.com>
2020-06-01 13:03:49 +0200

    Add login page styles

---
+++ frontend/myeats/src/components/forms/LoginForm.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="login-form">
+        <div class="form-wrapper">
+            <img src="../../assets/img/logo.png" alt="My Eats Scheduler">
+            <h1>{{ $t('loginPage.signIn') }}</h1>
+            <form>
+                <div class="form-group">
+                    <label for="user-input">
+                        <span>{{ $t('loginPage.inputEmail') }}</span>
+                        <input name="user-input" type="text" autocomplete="off" />
+                    </label>
+                </div>
+
+                <div class="form-group">
+                    <label for="psw-input">
+                        <span>{{ $t('loginPage.inputPass') }}</span>
+                        <input name="psw-input" type="password" />
+                    </label>
+                </div>
+
+                <div class="form-group">
+                    <div class="remember">
+                        <input name="keep-login-input" type="checkbox" />
+                        <span>{{ $t('loginPage.inputKeepLogin') }}</span>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="submit">
+                        <input name="submit-input" type="submit" :value="$t('loginPage.inputSubmit')" />
+                    </div>
+                </div>
+            </form>
+            
+            <div class="form-links">
+                <router-link :to="$i18nRoute({ name: 'Home' })">
+                    {{ $t('loginPage.passwordRecover') }}
+                </router-link>
+                <router-link :to="$i18nRoute({ name: 'Home' })">
+                    {{ $t('loginPage.createAccount') }}
+                </router-link>
+                <router-link :to="$i18nRoute({ name: 'Home' })">
+                    {{ $t('loginPage.goBackHome') }}
+                </router-link>
+            </div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm'
+}
+</script>
--- frontend/myeats/src/views/Login.vue
+++ frontend/myeats/src/views/Login.vue
@@ -17,24 +17,18 @@
             <img src="../assets/img/login.jpg" alt="Sign in">
         </div>
         
-        <div class="form">
-            <img src="../assets/img/logo.png" alt="My Eats Scheduler">
-            <h1>Sign in</h1>
-            <form action="">
-                <input type="text">
-            </form>
-            
-            <router-link :to="$i18nRoute({ name: 'Home' })">
-                {{ $t('mainMenu.home') }}
-            </router-link>
-        </div>
+        <LoginForm />
     </div>
 </template>
 
 <script>
+import LoginForm from '@/components/forms/LoginForm.vue'
 
 export default {
   name: 'login',
+  components: {
+      LoginForm
+  },
   data() {
       return {
           
@@ -45,23 +39,99 @@ export default {
 
 <style scoped>
 .img-form-wrapper {
-    @apply flex items-stretch -mx-3
+    @apply relative text-gray-800;
+    min-height: calc(100vh)
 }
 
-.img-form-wrapper .image {
+.img-form-wrapper >>> .login-form {
+    @apply pt-0 absolute z-0 inset-0 pt-10 w-full text-center
+}
 
+.img-form-wrapper >>> .login-form .form-wrapper {
+    @apply bg-gray-400 bg-opacity-75 w-11/12 p-4 mx-auto rounded-lg shadow-lg border border-gray-700
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper form .form-group {
+    @apply mb-2
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper form .form-group label {
+    @apply flex flex-col max-w-sm mx-auto bg-blue-300 rounded-t-md rounded-b-md border-2 border-blue-300
+}
+.img-form-wrapper >>> .login-form .form-wrapper form .form-group label span {
+    @apply py-1 px-2 pb-0 text-left text-xs text-gray-600
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper form .form-group label input {
+    @apply py-1 px-2 bg-blue-300 rounded-b-md
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper form input:focus {
+    @apply outline-none shadow-md
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper form .form-group .remember {
+    @apply flex items-center justify-center
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper form .form-group .remember input {
+    @apply mr-2
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper form .form-group .remember span {
+    @apply text-sm
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper form .form-group .submit input {
+    @apply py-2 px-6 bg-blue-900 text-blue-200 text-lg border-2 border-blue-900 rounded-md
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper form .form-group .submit input:hover {
+    @apply outline-none bg-indigo-200 text-blue-900 shadow-md cursor-pointer
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper .form-links {
+    @apply flex flex-col items-center text-xs mt-3
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper .form-links a:hover {
+    @apply text-blue-700
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper img{
+    @apply mx-auto w-56 mb-5
+}
+
+.img-form-wrapper >>> .login-form .form-wrapper h1{
+    @apply mb-3
 }
 
 .img-form-wrapper .image img{
-    @apply object-cover
+    @apply object-cover object-right;
+    min-height: calc(100vh);
 }
 
 @media (min-width: 640px) {
+    .img-form-wrapper >>> .login-form .form-wrapper {
+        @apply w-8/12 
+    }
+}
+
+@media (min-width: 768px) {
     .img-form-wrapper {
-        @apply -mx-12
+        @apply flex items-center flex-row-reverse bg-blue-900;
+        height: calc(100vh)
     }
 
-    .img-form-wrapper .image, .img-form-wrapper .form {
+    .img-form-wrapper >>> .login-form {
+        @apply relative w-auto pt-0
+    }
+
+    .img-form-wrapper >>> .login-form .form-wrapper {
+        @apply bg-opacity-100 bg-blue-200 w-11/12 border-0
+    }
+
+    .img-form-wrapper .image, .img-form-wrapper .login-form {
         @apply w-1/2
     }
 }
