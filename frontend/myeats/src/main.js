d1e69bb43a993622d31da21792b2a5678959bf0b
zeppelin17 <polmonclus@outlook.com>
2020-06-05 06:46:01 +0200

    Install vue-axios

--- frontend/myeats/src/main.js
+++ frontend/myeats/src/main.js
@@ -5,10 +5,12 @@
  * @author Zeppelin17 <elzeppelin17@gmail.com>
  *
  * Created at     : 2020-04-29 16:38:01 
- * Last modified  : 2020-05-30 08:59:06
+ * Last modified  : 2020-06-05 06:44:15
  */
 
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 import { i18n } from './i18n'
 import router from './router'
@@ -18,6 +20,8 @@ import '@/assets/css/tailwind.css'
 
 import Application from './layouts/Application.vue'
 import Website from './layouts/Website.vue'
+
+Vue.use(VueAxios, axios)
 
 Vue.prototype.$i18nRoute = Trans.i18nRoute.bind(Trans)
 
