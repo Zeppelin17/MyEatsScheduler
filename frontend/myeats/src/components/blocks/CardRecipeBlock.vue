56e9bac266c5dae20dc83d58686adf280fad715a
zeppelin17 <polmonclus@outlook.com>
2020-06-26 06:45:47 +0200

    Add card block for recipes

---
+++ frontend/myeats/src/components/blocks/CardRecipeBlock.vue
@@ -0,0 +1,118 @@
+<script>
+/**
+ * Block component to show a card of a recipe
+ *
+ * @summary Block component to show a card of a recipe
+ * @author Zeppelin17 <elzeppelin17@gmail.com>
+ *
+ * Created at     : 2020-06-25 06:28:12
+ * Last modified  : 2020-06-26 06:45:08
+ */
+</script>
+<template>
+  <div class="card">
+    <div class="name t-all-ease">
+        <p>{{ name }}</p>
+    </div>
+    <div class="content-wrapper">
+      <div class="content">
+        <span>{{ $t('appPages.recipes.categories') }}: </span>
+        <span class="tag t-all-ease" v-for="(category, index) in categories" :key="index">{{ category }}</span>
+      </div>
+      <div class="content">
+        <span>{{ $t('appPages.recipes.ingredients') }}: </span>
+        <span class="tag t-all-ease">{{ ingredients }}</span>
+      </div>
+      
+      <div class="action">
+        <router-link :to="$i18nRoute({ name: 'Dashboard' })" class="button t-all-ease">
+          {{ $t('appPages.recipes.openRecipe') }}
+        </router-link>
+
+        <a href="#" class="button danger t-all-ease">
+          {{ $t('appPages.recipes.deleteRecipe') }}
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardRecipeBlock',
+  props: {
+    id: {
+      type: String,
+      required: true,
+    }, 
+    name: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: true,
+    },
+    ingredients: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.t-all-ease {
+  transition: all .5s ease;
+}
+
+.card {
+  @apply w-full bg-blue-300 rounded-lg overflow-hidden shadow-xl
+}
+
+.card .content-wrapper {
+  @apply p-4
+}
+
+.card .content-wrapper .content {
+  @apply mb-6
+}
+
+.card .content-wrapper .tag {
+  @apply py-1 px-2 bg-blue-700 rounded-md mx-1 text-xs text-blue-300
+}
+
+.card:hover .content-wrapper .tag {
+  @apply text-blue-100
+}
+
+.card .name{
+  @apply px-3 py-10 bg-blue-900 border-blue-900 border-b-4 text-xl leading-tight font-bold text-blue-200 overflow-hidden
+}
+
+.card:hover .name{
+  @apply bg-blue-500 text-blue-100
+}
+
+.card .action {
+  @apply flex items-center justify-around mt-6 mb-1
+}
+
+.card .action .button {
+  @apply block w-full p-1 m-2 text-center text-sm rounded-md bg-blue-900 text-blue-200 border-2 border-blue-900 outline-none align-bottom
+}
+
+.card .action .button:hover {
+  @apply bg-blue-200 text-blue-900 font-bold
+}
+
+
+
+
+@media (min-width: 640px) {
+  .card {
+    @apply w-1/3 max-w-xs
+  }
+}
+</style>
--- frontend/myeats/src/views/app/Recipes.vue
+++ frontend/myeats/src/views/app/Recipes.vue
@@ -6,20 +6,27 @@
  * @author Zeppelin17 <elzeppelin17@gmail.com>
  *
  * Created at     : 2020-06-24 09:06:55
- * Last modified  : 2020-06-24 09:15:24
+ * Last modified  : 2020-06-26 06:21:19
  */
 </script>
 
 
 <template>
     <div class="recipes">
+      <h1>{{ $t('appPages.recipes.mainTitle') }}</h1>
 
-        
+      <CardRecipeBlock
+        id="1" 
+        name="My first recipe"
+        :categories="['categ_1', 'categ_2', 'categ_3']"
+        ingredients="6"
+      />
 
     </div>
 </template>
 
 <script>
+import CardRecipeBlock from '@/components/blocks/CardRecipeBlock.vue'
 
 export default {
   name: 'recipes',
@@ -29,10 +36,13 @@ export default {
       }
   },
   components: {
-      
+      CardRecipeBlock
   }
 }
 </script>
 
 <style scoped>
+.recipes {
+  @apply text-center
+}
 </style>
