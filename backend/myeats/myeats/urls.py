0a49d0ca56dde6ef975b3e1c3969ef079fd12573
zeppelin17 <polmonclus@outlook.com>
2020-08-08 18:11:05 +0200

    Override ObtainAuthToken class to add id from user in authentication response

--- backend/myeats/myeats/urls.py
+++ backend/myeats/myeats/urls.py
@@ -6,7 +6,7 @@
  * @author Zeppelin17 <elzeppelin17@gmail.com>
  *
  * Created at     : 2020-04-18 11:08:59 
- * Last modified  : 2020-04-25 11:49:26
+ * Last modified  : 2020-08-08 17:50:43
  */
  """
  
@@ -14,6 +14,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from rest_framework.authtoken import views
+from myeats_user.views import CustomAuthToken
 from rest_framework.schemas import get_schema_view
 from django.views.generic import TemplateView
 from rest_framework import permissions
@@ -39,7 +40,7 @@ urlpatterns = [
     url(r'^api/docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     
     path('admin-myeats/', admin.site.urls),
-    url('api/auth/', views.obtain_auth_token),
+    url('api/auth/', CustomAuthToken.as_view()),
     path('', include('myeats_user.urls')),
     path('', include('myeats_scheduler.urls')),
     path('', include('myeats_recipes.urls'))
--- backend/myeats/myeats_user/views.py
+++ backend/myeats/myeats_user/views.py
@@ -6,7 +6,7 @@
  * @author Zeppelin17 <elzeppelin17@gmail.com>
  *
  * Created at     : 2020-04-19 06:36:02 
- * Last modified  : 2020-06-13 06:53:46
+ * Last modified  : 2020-08-08 17:47:39
  */
 """
 
@@ -15,6 +15,23 @@ from myeats_user.serializers import MyEatsUserSerializer
 from rest_framework import generics, permissions
 from rest_framework.authentication import TokenAuthentication
 from myeats_user.permissions import IAmThisUser
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key,
+            'user_id': user.pk
+        })
 
 class MyEatsUserCreate(generics.CreateAPIView):
     queryset = MyEatsUser.objects.all()
