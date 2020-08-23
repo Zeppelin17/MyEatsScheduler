4dafe1ddd3e326bed0df77108eb6f31fbf92dd19
zeppelin17 <polmonclus@outlook.com>
2020-08-23 06:37:42 +0200

    Update styles in page action buttons

--- frontend/myeats/src/components/AppPageActionButtons.vue
+++ frontend/myeats/src/components/AppPageActionButtons.vue
@@ -31,8 +31,7 @@ export default {
          * Actions array item example:
          * {
          *      type: "create" or "edit" or "delete"
-         *      entity: "Recipe"
-         *      text: "create new"
+         *      entity: "Recipe/Week/etc"
          *      onClick: "method_name"  // parent method to call on click
          * }
          */
@@ -62,14 +61,29 @@ export default {
 }
 
 .page-action-buttons .action-btn {
-    @apply w-auto my-1 mx-2 p-2 rounded-md text-sm text-blue-200 bg-blue-900 border-blue-900 border-2 outline-none whitespace-no-wrap
+    @apply w-auto my-1 mx-1 p-2 rounded-md text-sm outline-none text-blue-200 bg-blue-900 border-blue-900 border-2 outline-none whitespace-no-wrap
 
 }
 
 .page-action-buttons .action-btn:hover {
-    @apply bg-blue-200 text-blue-900 font-bold outline-none
+    @apply bg-blue-200 text-blue-900 shadow-md
 }
 
+.page-action-buttons .action-btn.edit {
+    @apply bg-blue-700 border-blue-700
+}
+
+.page-action-buttons .action-btn.edit:hover {
+    @apply bg-blue-200 text-blue-700
+}
+
+.page-action-buttons .action-btn.delete {
+    @apply bg-red-500 border-red-500
+}
+
+.page-action-buttons .action-btn.delete:hover {
+    @apply bg-red-200 text-red-600
+}
 
 @media (min-width: 640px) {
     .page-action-buttons {
